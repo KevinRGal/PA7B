c4493c64e8f6976cf10ed02ada0ad9cca846d274
unknown <kevin.galicia@alumno.uttehuacan.edu.mx>
2018-10-04 16:14:18 -0500

    October 2nd

---
+++ POO/Dog.java
@@ -0,0 +1,30 @@
+
+
+public class Dog {
+  //Properties or attributes
+  public String color;
+  public String eyeColor;
+  public Double height;
+  public Double leight;
+  public Double weight;
+
+  //Methods o behaviors
+  public String sit(){
+    return "estoy sentado";
+    //System.out.println("estoy sentado");
+  }
+  public void bark(){
+
+    System.out.println("wauff wauff wauff");
+  }
+  public void layDown(){
+    System.out.println("estoy recostado");
+  }
+  public void eat(){
+    System.out.println("I am eating");
+  }
+  public void sleep(){
+    System.out.println("zzzZZZ");
+  }
+
+}
